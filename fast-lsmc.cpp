19d5fee8fa4c369a30cc1ad091b32ddd6e440b58
colalb1 <Colin.albs@gmail.com>
2024-04-22 16:14:36 -0500

    Minor cleanliness improvements. Implemented condition that instead of S - K > 0, I evaluate on if current rebate + discounted cashflow up to exercise > next exercise date. Added call flag, so now have American put AND call.

--- fast-lsmc.cpp
+++ fast-lsmc.cpp
@@ -21,7 +21,7 @@ default_random_engine generator;
 
 // Initializing variables
 double r, K, S_0, T, sigma;
-int num_trials, num_divisions, num_sims;
+int num_trials, num_divisions, num_sims, call_flag;
 
 // Max and min functions
 double max(double a, double b) {
@@ -73,6 +73,7 @@ int main(int argc, char* argv[]) {
 	sscanf(argv[5], "%lf", &K);				// strike price
 	sscanf(argv[6], "%d", &num_divisions);	// # grid steps
 	sscanf(argv[7], "%d", &num_sims);		// number simulations
+	sscanf(argv[8], "%d", &call_flag);		// call (1) or put (0)
 
     // defining time, rate, and SD step lengths
     double dt = T / ((double) num_divisions);
@@ -82,7 +83,7 @@ int main(int argc, char* argv[]) {
 
     // printing arguments (sanity check)
     cout << "--------------------------------" << endl;
-	cout << "American Put Option Pricing using Longstaff and Schwartz's Least Squares Monte Carlo Simulation" << endl;
+	cout << "American Option Pricing using (Modified) Longstaff and Schwartz's Least Squares Monte Carlo Simulation" << endl;
 	cout << "Expiration Time (Years) = " << T << endl;
 	cout << "Risk Free Interest Rate = " << r << endl;
 	cout << "Volatility (%age of stock value) = " << sigma * 100 << endl;
@@ -91,13 +92,14 @@ int main(int argc, char* argv[]) {
 	cout << "Number of Simulations = " << num_sims << endl;
 	cout << "Number of Divisions = " << num_divisions << endl;
 	cout << "R = " << R << endl;
+	cout << "Is Call? = " << call_flag << endl;
 	cout << "--------------------------------" << endl;
     
     // defining American put price
-    double put_price = 0.0;
+    double option_price = 0.0;
 
-    // iterating through blocks, this is to limit memory usage since solving using num_sims would use big matrices
-	// the outcome is the same doing it this way
+    // Iterating through blocks, this is to limit memory usage since solving using num_sims would use big matrices.
+	// The outcome is the same doing it this way.
     for (int k = 0; k <= (num_sims / 200); k++) {
 		
 		// setting trial number
@@ -110,10 +112,12 @@ int main(int argc, char* argv[]) {
 		if (num_trials > 0) {
 			double asset_price[num_trials][num_divisions];
 
+			// Setting initial price
 			for (int i = 0; i < num_trials; i++) {
                 asset_price[i][0] = S_0;
             }
 			
+			// Generating random path
 			for (int i = 0; i < num_trials; i++) {
                 for (int j = 1; j < num_divisions; j++) {
                     asset_price[i][j] = asset_price[i][j - 1] * exp(dR + dSD * get_gaussian());
@@ -122,31 +126,64 @@ int main(int argc, char* argv[]) {
             
 			double value[num_trials];
 
-			// initialize the value based on the price at final stage
+			// Setting last column to payoff instead of price
 			for (int i = 0; i < num_trials; i++) {
-                value[i] = max(0.0, K - asset_price[i][num_divisions - 1]);
+				if (call_flag == 1) {
+					value[i] = max(0.0, asset_price[i][num_divisions - 1] - K);
+				} else {
+					value[i] = max(0.0, K - asset_price[i][num_divisions - 1]);
+				}
             }
 			
 			for (int i = (num_divisions - 1); i > 0; i--) {
 				Eigen::MatrixXd independent_vars(num_trials, 1);
 				Eigen::MatrixXd dependent_vars(num_trials, 1);
 
-				int num_vars = 0;
+				int num_paths = 0;
 
+				// Choosing which values to use for regression/interpolation
+				// Adapting this to non-zero cashflow. If current rebate + discounted cashflow up to exercise < discounted min rebate
+				// at next exercise date, then do not evaluate. This is changed from if K - S_i > 0 then keep.
+
+				// Taken from last sentence of first paragraph of page 12 of:
+				// https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1331904
 				for (int j = 0; j < num_trials; j++) {
-					if (max(0.0, K - asset_price[j][i]) > 0) {
-						num_vars++;
-						independent_vars(num_vars, 0) = asset_price[j][i];
-						dependent_vars(num_vars, 0) = value[j] / R;
+					if (call_flag == 1) {
+						if ((asset_price[j][i] - K) + (asset_price[j][i + 1] - K) / R * exp(r * i / num_divisions * T) < 
+							(asset_price[j][i + 1] - K) / R * exp(r * (i + 1) / num_divisions * T)) {
+							num_paths++;
+							independent_vars(num_paths, 0) = asset_price[j][i];
+							dependent_vars(num_paths, 0) = value[j] / R;
+						}
+
+						// I spent so long fucking around with this if condition and didn't realize I flipped the sign
+						// if (asset_price[j][i] - K < 0) {
+						// 	num_paths++;
+						// 	independent_vars(num_paths, 0) = asset_price[j][i];
+						// 	dependent_vars(num_paths, 0) = value[j] / R;
+						// }
+					} else {
+						if ((K - asset_price[j][i]) + (K - asset_price[j][i + 1]) / R * exp(r * i / num_divisions * T) > 
+							(K - asset_price[j][i + 1]) / R * exp(r * (i + 1) / num_divisions * T)) {
+							num_paths++;
+							independent_vars(num_paths, 0) = asset_price[j][i];
+							dependent_vars(num_paths, 0) = value[j] / R;
+						}
+						// if (K - asset_price[j][i] > 0) {
+						// 	num_paths++;
+						// 	independent_vars(num_paths, 0) = asset_price[j][i];
+						// 	dependent_vars(num_paths, 0) = value[j] / R;
+						// }
 					}
 				}
 				
-				if (num_vars > 0) {
+				if (num_paths > 0) {
 					// regressing the dependent_variables on the independent variables using a 4th order polynomial
-					Eigen::MatrixXd a(min(num_vars, 5), 1);
-					a = polynomial_regression(independent_vars, dependent_vars, min(5, num_vars), num_vars);
-                    int poly_degree = min(num_vars, 5);
-
+					// Laguerre basis; change this later. This provides a lower bound (for call) but not an upper bound (for call)
+					int poly_degree = min(num_paths, 10);
+					Eigen::MatrixXd a(poly_degree, 1);
+					a = polynomial_regression(independent_vars, dependent_vars, poly_degree, num_paths);
+					
 					for (int j = 0; j < num_trials; j++) {
 						double poly_eval = 0;
 
@@ -154,28 +191,37 @@ int main(int argc, char* argv[]) {
 							poly_eval += a(l, 0) * pow(asset_price[j][i], l);
 						}
 						
-						if (((K - asset_price[j][i]) > poly_eval) && ((K - asset_price[j][i]) > 0.0)) {
-							value[j] = K - asset_price[j][i];
+						if (call_flag == 1) {
+							if (((asset_price[j][i] - K) > poly_eval) && ((asset_price[j][i] - K) > 0.0)) {
+								value[j] = asset_price[j][i] - K;
+							} else {
+								value[j] /= R;
+							}
 						} else {
-							value[j] /= R;
+							if (((K - asset_price[j][i]) > poly_eval) && ((K - asset_price[j][i]) > 0.0)) {
+								value[j] = K - asset_price[j][i];
+							} else {
+								value[j] /= R;
+							}
 						}
 					}
 				}
 			}
 			
-			double local_put_price = 0.0;
+			double local_option_price = 0.0;
 
 			for (int j = 0; j < num_trials; j++) {
-                local_put_price += value[j];
+                local_option_price += value[j];
             }
 
-			local_put_price = (local_put_price / ((double) num_trials)) / R;
-			put_price += local_put_price;
+			local_option_price /= ((double) num_trials) * R;
+			option_price += local_option_price;
 		}
 	}
 	
 	if (num_sims % 200 == 0) 
-		cout << "American Put Price = " << put_price / ((double) num_sims / 200)  << endl;
+		cout << "American Option Price = " << option_price / ((double) num_sims / 200)  << endl;
 	else 
-		cout << "Put Price = " << put_price / ((double) num_sims / 200 + 1) << endl;
+		cout << "Option Price = " << option_price / ((double) num_sims / 200 + 1) << endl;
 }
+// Hypothetically, this should be more accurate and faster than the standard LS. I'm a complete imbecile so maybe not.

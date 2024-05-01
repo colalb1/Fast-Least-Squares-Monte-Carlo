fd08c5c5d3b076847cebf68cf0295352adcd08eb
colalb1 <Colin.albs@gmail.com>
2024-05-01 16:42:42 -0500

    Began integration of original code to header file. Most time has been spent debugging due to indexing issues with Eigen. Next: Debug original method and test. Write csv intake and output for testing.

--- generate-data.cpp
+++ generate-data.cpp
@@ -7,10 +7,12 @@
 #include <algorithm>
 #include <chrono>
 #include "fast-lsmc.hpp"
+// #include "original.hpp"
 
 int main() {
     // testing if header file works
-    cout << option_value(10.0, 0.1, 0.4, 100, 150, 1000, 10000, 0, "Hermitian") << endl;
+    // cout << option_value(10.0, 0.1, 0.4, 100, 150, 1000, 10000, 0, "Hermitian") << endl;
+    cout << option_value_original(10.0, 0.1, 0.4, 100, 150, 1000, 10000, 0) << endl;
 
 
     // NEED TO WRITE CSV FILE INPUT. SEE CHATGPT CSV FILE PROCESSING CODE
